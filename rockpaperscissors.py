282a8137b1e457af84abbb21831fa2d450e5f0c7
Amirhossein Moznebi <59997964+amirimoo@users.noreply.github.com>
2023-10-12 17:26:51 +0330

    Add Score and Positions

---
+++ rockpaperscissors.py
@@ -0,0 +1,10 @@
+import random
+
+#Start Score and Positions
+my_score       = 0 
+computer_score = 0
+
+positions = ["Rock" , "Paper" , "Scissors"]
+
+Random_Position = random.randint(0,2)
+#End Score and Positions
